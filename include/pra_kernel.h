61470ee078c9f83d971263b04531ffa07bbc10d9
Vladimir Bieleny <bielenyv@gmail.com>
2022-05-12 16:36:43 +0200

    Add more comments and fix Doxygen to add README

--- include/pra_kernel.h
+++ include/pra_kernel.h
@@ -1,5 +1,13 @@
+/**
+ * @file
+ * @brief Specifies a kernel function that will be run as soon as bootloader exits.
+ */
+
 #pragma once
 
 #include "pra_attrs.h"
 
+/**
+ * @brief Main kernel function that will be executed from bootloader.
+ */
 PRA_UNUSED void kernel_main() PRA_SECTION(".text.kernel");
--- include/pra_kmalloc.h
+++ include/pra_kmalloc.h
@@ -1,8 +1,22 @@
+/**
+ * @file
+ * @brief Functionality of a kernel memory allocator used for kernel code.
+ */
+
 #pragma once
 
 #include <stddef.h>
 
+/**
+ * @brief Initializes a kernel memory allocator by providing a free memory block.
+ * @param memory_start starting address of a free memory block
+ * @param memory_size_bytes size of the free block in bytes
+ */
 void kernel_memory_initialize(void *memory_start, size_t memory_size_bytes);
+
+/**
+ * @brief 
+ */
 void* kernel_memory_allocate(size_t bytes, size_t alignment);
 void* kernel_memory_reallocate(void *allocated_memory, size_t memory_new_size_bytes, size_t alignment);
 void kernel_memory_free(void *allocated_memory);
